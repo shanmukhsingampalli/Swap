ddce5fa6a1b1e7b4c16a9e456dbdb6b5b60b667b
shanmukhsingampalli <shanmukhsingampalli9@gmail.com>
2025-04-06 23:05:31 +0530

    Fix app initialization order in server.js

--- backend/server.js
+++ backend/server.js
@@ -6,11 +6,12 @@ const mongoose = require("mongoose");
 const workoutRoutes = require("./routes/workouts");
 const userRoutes = require("./routes/user");
 const path = require("path");
+const app = express();
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
 });
-const app = express();
+
 app.use(cors({ origin: "*" }));
 
 app.use(express.json());
