8521ef277f313958ea5ef986bfb4df74003cc901
shanmukhsingampalli <shanmukhsingampalli9@gmail.com>
2025-04-06 23:18:30 +0530

    Fix file path for serving frontend index.html in server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const path = require("path");
 const app = express();
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+  res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
 });
 
 app.use(cors({ origin: "*" }));
